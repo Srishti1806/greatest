50f21324bd46cf560666322491af44a221efb772
Srishti1806 <srishtinaikar620@gmail.com>
2026-01-18 21:31:33 +0530

    Save

---
+++ greatest.py
@@ -0,0 +1,10 @@
+def max(num1,num2,num3,result):
+    num1=int(input("Enter num1:"))
+    num2=int(input("Enter num2:"))
+    num3=int(input("Enter num3:"))
+    result=max(num1,num2,num3)
+if __name__ == "__main__":
+    Num1 = 10
+    Num2 = 100
+    Num3 = 50
+print(max(10,100,50,100))        
---
+++ test_greatest.py
@@ -0,0 +1,8 @@
+from greatest import max
+def test_max_output():
+    expected_output = (
+        "Num1 = 10\n"
+        "Num2 = 100\n"
+        "Num3 = 50\n"
+    )
+    assert max (num1,num2,num3,result) == expected_output
