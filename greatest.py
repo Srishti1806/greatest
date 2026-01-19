749e2856081535d11a9dfb35296ec0a54c3041eb
Srishti1806 <srishtinaikar620@gmail.com>
2026-01-19 09:49:09 +0530

    Update greatest.py

--- greatest.py
+++ greatest.py
@@ -1,11 +1,11 @@
-def max(Num1,Num2,Num3,Result):
-    Num1=int(input("Enter num1:"))
-    Num2=int(input("Enter num2:"))
-    Num3=int(input("Enter num3:"))
-    Result=max(Num1,Num2,Num3)
+def max(Num1, Num2, Num3, Result):
+    Num1=int(input("Enter Num1:"))
+    Num2=int(input("Enter Num2:"))
+    Num3=int(input("Enter Num3:"))
+    Result=max(Num1, Num2, Num3)
 if __name__ == "__main__":
     Num1 = 10
     Num2 = 100
     Num3 = 50
     Result = 100
-print(max(Num1,Num2,Num3,Result))        
+print(max(Num1, Num2, Num3, Result))        
