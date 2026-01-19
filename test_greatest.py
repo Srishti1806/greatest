938ea576a3b8fdfd948eb1fc34909825770046bb
Srishti1806 <srishtinaikar620@gmail.com>
2026-01-19 09:42:26 +0530

    Update test_greatest.py

--- test_greatest.py
+++ test_greatest.py
@@ -1,8 +1,9 @@
 from greatest import max
 def test_max_output():
     expected_output = (
-        "Num1 = 10\n"
-        "Num2 = 100\n"
-        "Num3 = 50\n"
+        "num1 = 10\n"
+        "num2 = 100\n"
+        "num3 = 50\n"
+        "result = 100\n"
     )
     assert max (num1,num2,num3,result) == expected_output
